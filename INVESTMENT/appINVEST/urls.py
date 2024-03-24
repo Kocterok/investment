529a7ff385f8654640b56cca9f7e0bc1396ea2a2
Cerrer <154823475+Cerrer@users.noreply.github.com>
2024-03-25 00:34:45 +0300

    URLS_VIEWS

--- INVESTMENT/appINVEST/urls.py
+++ INVESTMENT/appINVEST/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.paageHome.as_view())
-    ]
+    path('', views.index),
+    path('about', views.abaut),
+]
