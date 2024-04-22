4bea06b08166f7644a71989dfd8431033931797d
Cerrer <154823475+Cerrer@users.noreply.github.com>
2024-04-23 01:49:36 +0300

    simbios

--- INVESTMENT/appAuth/views.py
+++ INVESTMENT/appAuth/views.py
@@ -49,3 +49,46 @@ class Logout(View):
     def get(self, request):
         logout(request)
         return redirect('urlAuthPage')   
+    
+    
+    
+    
+    
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+# from django.contrib.auth import authenticate, login, logout
+
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('portfolio')
+#     else:
+#         form = AuthenticationForm()
+#     return render(request, 'login.html', {'form': form})
+
+# def logout_view(request):
+#     logout(request)
+#     return redirect('login')
+
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('portfolio')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'register.html', {'form': form})
+
+# @login_required
+# def portfolio(request):
+#     # Код представления для портфеля
+#     pass
